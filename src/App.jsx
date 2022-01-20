1209004adf73affb1a55e78c35e9408304d8e770
Iury Roque <iury.roque@hotmail.com>
2022-01-19 21:47:19 -0300

    Fix: App css adjust

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Table from './components/Table';
 import Header from './components/Header';
 import Myprovider from './context/PlanetsProvider';
+import './App.css';
 
 function App() {
   return (
+
     <Myprovider>
-      <Header />
-      <Table />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="table">
+        <Table />
+      </div>
     </Myprovider>
   );
 }
